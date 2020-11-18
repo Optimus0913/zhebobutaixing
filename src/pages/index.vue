6f7a4953fa6ed5248b109730c406b0e99856612b
yuduoduo <18756697012@163.com>
2020-11-18 09:02:16 +0800

    难顶

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,6 +10,7 @@
       </el-header>
       <el-container>
         <el-aside width="200px"><navv></navv></el-aside>
+        <div id="main" style="width: 100%; height: 500px"></div>
         <el-main>
           <router-view></router-view>
         </el-main>
@@ -19,6 +20,7 @@
 </template>
 
 <script>
+import e from "echarts";
 import { mapGetters, mapActions } from "vuex";
 import navv from "@/components/nav";
 export default {
@@ -31,6 +33,77 @@ export default {
   computed: {
     ...mapGetters(["getUserinfo"]),
   },
+  mounted() {
+    let echars = e.init(document.getElementById("main"));
+    var option = {
+      backgroundColor: "#2c343c",
+
+      title: {
+        text: "总来访量",
+        left: "center",
+        top: 20,
+        textStyle: {
+          color: "#ccc",
+        },
+      },
+
+      tooltip: {
+        trigger: "item",
+        formatter: "{a} <br/>{b} : {c} ({d}%)",
+      },
+
+      visualMap: {
+        show: false,
+        min: 80,
+        max: 600,
+        inRange: {
+          colorLightness: [0, 1],
+        },
+      },
+      series: [
+        {
+          name: "访问来源",
+          type: "pie",
+          radius: "55%",
+          center: ["50%", "50%"],
+          data: [
+            { value: 335, name: "直接访问" },
+            { value: 310, name: "邮件营销" },
+            { value: 274, name: "联盟广告" },
+            { value: 235, name: "视频广告" },
+            { value: 400, name: "搜索引擎" },
+          ].sort(function (a, b) {
+            return a.value - b.value;
+          }),
+          roseType: "radius",
+          label: {
+            color: "rgba(255, 255, 255, 0.3)",
+          },
+          labelLine: {
+            lineStyle: {
+              color: "rgba(255, 255, 255, 0.3)",
+            },
+            smooth: 0.2,
+            length: 10,
+            length2: 20,
+          },
+          itemStyle: {
+            color: "#c23531",
+            shadowBlur: 200,
+            shadowColor: "rgba(0, 0, 0, 0.5)",
+          },
+
+          animationType: "scale",
+          animationEasing: "elasticOut",
+          animationDelay: function (idx) {
+            return Math.random() * 200;
+          },
+        },
+      ],
+    };
+    echars.setOption(option);
+  },
+
   methods: {
     ...mapActions(["changeUserInfoAction"]),
     logout() {
